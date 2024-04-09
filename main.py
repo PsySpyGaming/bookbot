c71ad10241f69b3681ea2928529e9a3f939947ed
PsySpyGaming <SConstance_IT@yahoo.com>
2024-04-08 21:01:12 -0500

    Currently on Step 16

---
+++ main.py
@@ -0,0 +1,26 @@
+def main():
+    book_path = "./books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+    print(get_chars_dict(text))
+    
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+main()
